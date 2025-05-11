40798821bab78bf4f4b1ff77407d5987f26c1f9b
CarmeloMelito <melitocarmelo211@gmail.com>
2025-05-11 19:55:59 +0200

    feat:express server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('web')
+const express = require("express");
+const app = express();
+//porta
+const port = 3000;
+//avvio server
+app.listen(port, () => {
+  console.log(`web app on port ${port}`);
+});
